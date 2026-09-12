1aeb4f8036aa4c502ad5fd377a5b243e7f6f89f6
anngello601 <anngello601@gmail.com>
2026-09-12 16:00:34 -0500

    nuevoRender

--- Biblioteca-Backend/src/main/java/com/example/Biblioteca/config/WebConfig.java
+++ Biblioteca-Backend/src/main/java/com/example/Biblioteca/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://biblioteca2-yh8o.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true) // 👈 ESTO ES CRUCIAL
                 .allowedHeaders("*");
